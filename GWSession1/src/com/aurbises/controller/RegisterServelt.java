fc6479456f0a1b7c94a528aada155fdd131d3938
Ishant Kumar <er.ishant@gmail.com>
2018-07-26 11:37:08 +0530

    Intro to Servlets

---
+++ GWSession1/src/com/aurbises/controller/RegisterServelt.java
@@ -0,0 +1,58 @@
+package com.aurbises.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.auribises.db.JDBCHelper;
+import com.auribises.model.User;
+
+
+@WebServlet({ "/RegisterServelt", "/Register", "/Register.do" })
+public class RegisterServelt extends HttpServlet {
+
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("==init executed==");
+	}
+
+	
+	public void destroy() {
+		System.out.println("==destroy executed==");
+	}
+
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("==service executed==");
+		
+		User user = new User();
+		user.name = request.getParameter("txtName");
+		user.email = request.getParameter("txtEmail");
+		user.password = request.getParameter("txtPassword");
+		
+		JDBCHelper helper = new JDBCHelper(); // Load the Driver
+		helper.createConnection();
+		int i = helper.registerUser(user);
+		helper.closeConnection();
+		
+		// To send back Response
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		
+		
+		out.print("<html>");
+		if(i>0){
+			out.print("<b>Thank you For Registering with Us !</b>");
+		}else{
+			out.print("<b>Registration Failed ! Please Try Again Later</b>");
+		}
+		out.print("</html>");
+		
+	}
+
+}
---
+++ GWSession1/src/com/auribises/db/JDBCHelper.java
@@ -0,0 +1,76 @@
+package com.auribises.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import com.auribises.model.User;
+
+
+public class JDBCHelper {
+	
+	Connection con;
+	PreparedStatement pStmt;
+
+	public JDBCHelper() {
+		
+		try {
+			
+			//1. Load the Driver
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("--Driver Loaded--");
+			
+		} catch (Exception e) {
+			System.out.println("Exception: "+e);
+		}
+		
+	}
+	
+	public void createConnection(){
+		try {
+			//2. Create the Connection
+			String url = "jdbc:mysql://localhost/GW2018AI";
+			String username = "root";
+			String password = "";
+			
+			con = DriverManager.getConnection(url, username, password);
+			System.out.println("--Connection Created--");
+			
+		} catch (Exception e) {
+			System.out.println("Exception: "+e);
+		}
+	}
+	
+	
+	public int registerUser(User uRef){
+		
+		int i = 0;
+		
+		try {
+			//3. Write SQL Statement
+			String sql = "insert into User values(null, ?, ?, ?)";
+			
+			//4. Execute SQL Statement
+			pStmt = con.prepareStatement(sql);
+			pStmt.setString(1, uRef.name);
+			pStmt.setString(2, uRef.email);
+			pStmt.setString(3, uRef.password);
+			
+			i = pStmt.executeUpdate();
+			
+		} catch (Exception e) {
+			System.out.println("Exception: "+e);
+		}
+		
+		return i;
+	}  
+	
+	public void closeConnection(){
+		try {
+			con.close();
+		} catch (Exception e) {
+			System.out.println("Exception: "+e);
+		}
+	}
+	
+}
